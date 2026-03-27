401e68d26c233bca3320bd52531567e037b4379a
Fizza Batool <fizzabatool066@gmail.com>
2026-03-27 18:21:11 +0500

    Authentication

--- server/server.js
+++ server/server.js
@@ -1,16 +1,26 @@
+// server.js
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 // Load env variables
 dotenv.config();
+
+// Database connect karo
+connectDB();
 
 // Express app initialize karein
 const app = express();
 
 // Middleware
-app.use(cors()); // Frontend ko allow karega
-app.use(express.json()); // JSON data read karne ke liye
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 // Basic Test Route
 app.get('/', (req, res) => {
