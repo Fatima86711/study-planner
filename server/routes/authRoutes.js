91b155f4eac400d13867979abc7405e060bef1d2
Fatima86711 <fatimazafar411133@gmail.com>
2026-03-27 19:35:13 +0500

    corrected error

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, getMe } = require('../controllers/authController');
+const { registerUser, loginUser, getMe } = require('../controller/authController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
